5811e1d16f6f8360876afeeb1081bc3bfd7ac801
OrpheeGT <OrpheeGT@users.noreply.github.com>
2023-03-01 00:20:33 +0100

    Update install.sh

--- i915-10th/install.sh
+++ i915-10th/install.sh
@@ -3,4 +3,6 @@
 if [ "${1}" = "late" ]; then
   echo "Installing 10th Gen iGPU firmware and drivers"
   tar -zxvf /addons/i915.tgz -C /tmpRoot/
+  rmmod i915
+  insmod /tmpRoot/usr/lib/modules/i915.3E98.ko
 fi
